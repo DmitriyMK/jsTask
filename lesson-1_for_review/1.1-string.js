2d0ccd056f3a918b51b50ee90db0f0b4e9931d53
alchimic <alchimicus@gmail.com>
2019-09-30 19:39:56 +0300

    add HW1 for review

---
+++ lesson-1_for_review/1.1-string.js
@@ -0,0 +1,52 @@
+let string = "some test string";
+
+
+// 1. Получить первую и последнюю буквы строки
+let firstLetter = string[0];
+let lastLetter = string[string.length - 1];
+let firstLastLetter = [firstLetter, lastLetter];
+console.log(`1. Получить первую и последнюю буквы строки - %c${firstLastLetter}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 2. Сделать первую и последнюю буквы в верхнем регистре
+let sliceString = string.slice(1, -1);
+let replaceFirstLastLetter = firstLetter.toUpperCase() + sliceString + lastLetter.toUpperCase();
+console.log(`2. Сделать первую и последнюю буквы в верхнем регистре - %c${replaceFirstLastLetter}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 3. Найти положение слова ‘string’ в строке
+let searchString = "string";
+let searchIndex = string.indexOf(searchString);
+let searchStringLength = searchString.length;
+let rangeString = (`Положение слова в строке с ${searchIndex} до ${searchIndex + searchStringLength}`);
+console.log(`3. Сделать первую и последнюю буквы в верхнем регистре - %c${replaceFirstLastLetter}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 4. Найти положение второго пробела (“вручную” ничего не считать)
+let firstSpace = string.indexOf(" ");
+let secondSpace = string.indexOf(" ", firstSpace + 1);
+console.log(`4. Найти положение второго пробела - %c${secondSpace}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 5. Получить строку с 5-го символа длиной 4 буквы
+let startSlice = 4;
+let sliceLength = Number(startSlice + 5);
+let spliceSomeWord = string.slice(startSlice, sliceLength);
+console.log(`5. Получить строку с 5-го символа длиной 4 буквы - %c${spliceSomeWord}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 6. Получить строку с 5-го по 9-й символы
+let spliceSomeWordNew = string.substring(4, 9);
+console.log(`6. Получить строку с 5-го по 9-й символы - %c${spliceSomeWordNew}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 7. Получить новую строку из исходной путем удаления последних 6-и символов
+// (то есть исходная строка без последних 6и символов)
+let newString = string.slice(0, -6);
+console.log(`7. Получить новую строку из исходной путем удаления последних 6-и символов - %c${newString}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016”
+let a = 20, b = 16;
+let strings = String(a) + b;
+console.log(`8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016” - %c"${strings}"`, ' color: green; font-weight: 700; font-size: 16px;');
---
+++ lesson-1_for_review/1.2-math.js
@@ -0,0 +1,29 @@
+
+// 1. Получить число pi из Math и округлить его до 2 - х знаков после точки
+let somethingNumber = Math.PI.toFixed(2);
+console.log(`1. Получить число pi из Math и округлить его до 2 - х знаков после точки - %c${somethingNumber}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
+let minNumber = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+let maxNumber = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+console.log(`2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51 - %c${maxNumber} ${minNumber}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+// 3. Работа с Math.random:
+// a.Получить случайное число и округлить его до двух цифр после запятой
+// b.Получить случайное целое число от 0 до X.X - любое произвольное число.
+let somethingNumberRandomA = Math.random().toFixed(2);
+let somethingNumberRandomB = Math.floor(Math.random() * 10);
+console.log(`3. Работа с Math.random: - %c${somethingNumberRandomA} ${somethingNumberRandomB}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+// 4. Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду(1.3)
+let sum = (0.6 + 0.7);
+console.log(`4. Проверить результат вычисления 0.6 + 0.7: - %c${sum.toFixed(1)}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+// 5. Получить число из строки ‘100$’
+let numberFromString = parseInt("100$");
+console.log(`5. Получить число из строки ‘100$’: - %c${numberFromString}`, ' color: green; font-weight: 700; font-size: 16px;');
+
+
+
