0d3021127a4fc28ab70576476761204ef059d7e5
alchimic <alchimicus@gmail.com>
2019-10-12 09:55:30 +0300

    done some hometasl of HW4

--- lesson-4/function.js
+++ lesson-4/function.js
@@ -35,12 +35,12 @@ function getMyltiplyByTen() {
 
 
 function getUserData() {
-  return arr3.map(element => {return `${element.name} is ${element.age}`}).join(', ');
+  return arr3.map(element => { return `${element.name} is ${element.age}` }).join(', ');
 }
 
 
 function getReverseStr() {
-  return arr4.map(element => {return element.split('').reverse().join('')})
+  return arr4.map(element => { return element.split('').reverse().join('') })
 }
 
 
@@ -53,10 +53,10 @@ console.log(getNewValue(arr4, getReverseStr));
 
 
 
-// 2. Написать аналог метода every. Создайте функцию every, она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) вторым аргументом callback (обязательно проверьте что передана функция)
+// 2. Написать аналог метода every. 
+// Создайте функцию every, она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) вторым аргументом callback (обязательно проверьте что передана функция)
 // функция должна возвращать true или false в зависимости от результата вызова callback (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив. 
 
-// Что такое метод every можно прочитать здесь и ниже в презентации тоже о них идет речь.
 
 
 
@@ -64,23 +64,54 @@ console.log(getNewValue(arr4, getReverseStr));
 
 
 
-
-// На основе массива [1,2,3,5,8,9,10] сформировать новый массив,
+//1. На основе массива [1,2,3,5,8,9,10] сформировать новый массив,
 // каждый элемент которого будет хранить информацию о числе и его четности:
-// [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
+// {digit: 1, odd: true }, { digit: 2, odd: false }, { digit: 3, odd: true }
+
+let arrDigit = [1, 2, 3, 5, 8, 9, 10];
+
+
+function isOddFunction(arr) {
+
+  const isOdd = arr.map(num => {
+    if (num % 2 === 0) {
+      return { digit: num, odd: true }
+    } else {
+      return { digit: num, odd: false }
+    }
+  })
+
+  return isOdd;
+}
+
+
+// Checking
+console.log(isOddFunction(arrDigit));
 
 
 
-// Проверить, содержит ли массив [12, 4, 50, 1, 0, 18, 40] элементы, равные нулю. Если да - вернуть false.
+
+// 2. Проверить, содержит ли массив [12, 4, 50, 1, 0, 18, 40] элементы, равные нулю. Если да - вернуть false.
+let arrZero = [12, 4, 50, 1, 0, 18, 40];
+let isZeroHere = arrZero.some(x => x === 0);
+
+
+//checking 
+console.log(isZeroHere);
 
 
 
-// Проверить, содержит ли массив ['yes', 'hello', 'no', 'easycode', 'what'] хотя бы одно слово длиной больше 3х букв. Если да - вернуть true
+//3.  Проверить, содержит ли массив ['yes', 'hello', 'no', 'easycode', 'what'] хотя бы одно слово длиной больше 3х букв. Если да - вернуть true
+let searchLetters = ['yes', 'hello', 'no', 'easycode', 'what'];
+let isSomeLettersHere = searchLetters.filter(x => x.length > 3);
+
+
+// checking
+console.log(isSomeLettersHere);
 
 
 
-
-// Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
+//4. Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
 
 // [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
 // {char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
@@ -92,3 +123,30 @@ console.log(getNewValue(arr4, getReverseStr));
 
 // Подсказка: вначале отсортируйте массив по index, затем используйте reduce() для построения
 // строки
+
+
+// Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [  [14, 45],  [1],  ['a', 'c', 'd']  ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+// Есть массив объектов:
+// [
+//     {cpu: 'intel', info: {cores:2, сache: 3}},
+//     {cpu: 'intel', info: {cores:4, сache: 4}},
+//     {cpu: 'amd', info: {cores:1, сache: 1}},
+//     {cpu: 'intel', info: {cores:3, сache: 2}},
+//     {cpu: 'amd', info: {cores:4, сache: 2}}
+// ]
+
+// Отсортировать их по возрастающему количеству ядер (cores).
+
+
+// 3. Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
+
+// let products = [
+//     {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+//     {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+//     {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+//     {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+// ];
+
+// filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
+
+
