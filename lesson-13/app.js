ab43827455445f9d09397d4a16a27cbc25b65b7c
alchimic <alchimicus@gmail.com>
2019-11-15 18:47:11 +0200

    add basic

---
+++ lesson-13/app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+
+// Ваша задача написать приложение по распознаванию речи.
+// Вы говорите, а приложение отрисовывает на странице то, что вы сказали. 
+
+// SpeechRecognition в помощь.
+
+const speech = new SpeechRecognition();
+let voicesSpeech = [];
+
+
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+
+
+
+start.addEventListener('click', startListen);
+end.addEventListener('click', endListen);
+
+
+function startListen() {
+
+}
+
+
+function endListen() {
+  
+}
