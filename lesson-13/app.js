c4a9ca740f113c50a8905cd20aac508a6ff36fb0
alchimic <alchimicus@gmail.com>
2019-11-17 20:07:10 +0200

    update hW13

--- lesson-13/app.js
+++ lesson-13/app.js
@@ -3,9 +3,7 @@
 
 // Ваша задача написать приложение по распознаванию речи.
 // Вы говорите, а приложение отрисовывает на странице то, что вы сказали. 
-
 // SpeechRecognition в помощь.
-
 
 
 window.speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
@@ -14,31 +12,18 @@ const recognition = new window.speechRecognition();
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 const content = document.querySelector('.content');
-let finalTranscript = '';
 
 recognition.interimResults = true;
-recognition.maxAlternatives = 10;
-recognition.continuous = true;
+recognition.lang = 'en-US';
 
 
 start.addEventListener('click', startListen);
 stop.addEventListener('click', endListen);
 
 
-recognition.onresult = (event) => {
-  let interimTranscript = '';
-
-  for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-
-    let transcript = event.results[i][0].transcript;
-    if (event.results[i].isFinal) {
-      finalTranscript += transcript;
-    } else {
-      interimTranscript += transcript;
-    }
-  }
-
-  content.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+recognition.onresult = function (event) {
+  let text = event.results[0][0].transcript;
+  content.textContent = transcript;
 }
 
 
