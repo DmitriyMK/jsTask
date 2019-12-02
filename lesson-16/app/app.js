6bbb8f23dd2b10ed174cc5a63da503a61229fa93
alchimic <alchimicus@gmail.com>
2019-12-03 01:10:25 +0200

    start autocomplete

---
+++ lesson-16/app/app.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let result = document.querySelector('.search-result');
+let searchArr = ['cat', 'dog', 'chupacabra', 'crocodile', 'elephant', 'cow', 'raccoon', 'dragon', 'eagle', 'iguana', 'sphinx'];
+
+
