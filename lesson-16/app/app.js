8db1a51c749f57327c8ebf8644303ea67d758364
alchimic <alchimicus@gmail.com>
2019-12-05 00:19:21 +0200

    done autocomplete

--- lesson-16/app/app.js
+++ lesson-16/app/app.js
@@ -1,6 +1,37 @@
 'use strict'
 
-let result = document.querySelector('.search-result');
-let searchArr = ['cat', 'dog', 'chupacabra', 'crocodile', 'elephant', 'cow', 'raccoon', 'dragon', 'eagle', 'iguana', 'sphinx'];
+let input = document.getElementById('search-input');
+let result = document.getElementById('search-result');
+let searchArray = ['cat', 'dog', 'chupacabra', 'crocodile', 'elephant', 'cow', 'raccoon', 'dragon', 'eagle', 'iguana', 'sphinx'];
+
+
+function autoComplete(searchArray, wordToSearch) {
+  return searchArray.filter(search => search.toLowerCase().includes(wordToSearch.toLowerCase()))
+}
+
+
+function getValue(value) {
+  let data = autoComplete(searchArray, value);
+
+
+  data.forEach((item) => {
+    const itemResult = item;
+    const itemSearch = `<ul><li><a href="#"> ${itemResult} </a></li></ul>`;
+
+    result.insertAdjacentHTML('beforeend', itemSearch);
+  });
+}
+
+
+function clearValue() {
+  result.innerHTML = '';
+}
+
+
+input.addEventListener('keyup', function () {
+  clearValue();
+  getValue(this.value);
+});
+
 
 
